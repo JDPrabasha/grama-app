7a7dc8cd92c2b1a3bf165b948da7ca02cfad8e1c
Sribarathvajasarma <gsribarathvajasarma@gmail.com>
2022-11-14 01:19:19 -0800

    change request method

--- backend/police_check_api/route/policeCheckRoute.js
+++ backend/police_check_api/route/policeCheckRoute.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const policeCheckController = require('../controller/policeCheckController')
 
-router.route('/check').get(policeCheckController.policeCheck)
+router.route('/check').post(policeCheckController.policeCheck)
 router.route('/add').post(policeCheckController.addPoliceCheck)
 
 
