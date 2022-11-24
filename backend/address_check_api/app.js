312f43aefb64db09547ccaaad73c3150833969e0
Dulaj <shad3s.of.m3@gmail.com>
2022-11-24 09:46:36 +0530

    import cors

--- backend/address_check_api/app.js
+++ backend/address_check_api/app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const db = require("./db/db");
 const request = require("./models/Request");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const app = express();
